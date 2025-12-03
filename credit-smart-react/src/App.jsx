b12b7306fcd0e9db2d17d73a6b5a2195dd50c077
CamiloUdeA <andresc.lezcano@udea.edu.co>
2025-12-03 01:26:11 -0500

    fix: carpeta de nodos inadecuada

--- credit-smart-react/src/App.jsx
+++ credit-smart-react/src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Componentes
 import Header from "./components/Header";
-
-// Páginas
 import Home from "./pages/Home";
 import Simulador from "./pages/Simulador";
 import Solicitar from "./pages/Solicitar";
@@ -11,20 +8,16 @@ import Solicitar from "./pages/Solicitar";
 export default function App() {
   return (
     <BrowserRouter>
-       <header className="site-header">
-        <div className="container header-inner">
-          <h1 className="brand">CreditSmart</h1>
-          <nav className="main-nav">
-            <Link to="/">Inicio</Link>
-            <Link to="/simulador">Simulador</Link>
-            <Link to="/solicitar">Solicitar Crédito</Link>
-          </nav>
-        </div>
-      </header>
+      <Header />
 
       <Routes>
+        {/* Página principal */}
         <Route path="/" element={<Home />} />
+
+        {/* Simulador */}
         <Route path="/simulador" element={<Simulador />} />
+
+        {/* Solicitar crédito */}
         <Route path="/solicitar" element={<Solicitar />} />
       </Routes>
     </BrowserRouter>
