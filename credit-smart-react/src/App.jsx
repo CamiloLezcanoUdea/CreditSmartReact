ea3e5e44adabc659421117b258ab5fa631915c3d
CamiloUdeA <andresc.lezcano@udea.edu.co>
2025-12-02 22:40:36 -0500

    style: Estilos responsive y pulido general

--- credit-smart-react/src/App.jsx
+++ credit-smart-react/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Importar las páginas
+// Componentes
+import Header from "./components/Header";
+
+// Páginas
 import Home from "./pages/Home";
 import Simulador from "./pages/Simulador";
 import Solicitar from "./pages/Solicitar";
@@ -8,6 +11,8 @@ import Solicitar from "./pages/Solicitar";
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/simulador" element={<Simulador />} />
